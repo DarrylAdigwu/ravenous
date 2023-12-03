0ed2c6c119003ee79efa12bd771cfb9cfe09058f
dreal <Darryl.adigwu@gmail.com>
2023-12-02 21:09:26 -0800

    components and view folders added. Hardcoded Business component added

---
+++ components/business.js
@@ -0,0 +1,28 @@
+import React from "react";
+
+const Business = () => {
+  return (
+    <div>
+      
+      <img
+        src=""
+        alt=""
+      />
+
+      <h2 className="name">The Hideout</h2>
+
+      <div className="address">
+        <p className="city">Fontana</p>
+        <p className="state">CA</p>
+        <p className="zipcode">92336</p>
+      </div>
+
+      <div className="description">
+        <h3 className="category">Nigerian</h3>
+        <p className="rating">5.0 stars</p>
+        <p className="review-count">90 reviews</p>
+      </div>
+
+    </div>
+  )
+};
